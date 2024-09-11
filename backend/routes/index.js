df4a8247a267e224f8fe304c2f00aa405d6b07b8
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-11 21:34:57 +0530

    Step 12 - Created a new router for accounts(account.js)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const userRouter = require('./user')
+const accountRouter = require('./account')
+
 
 // api/v1/user/(anything) goes to userRouter defined in ./user.js
-router.use('/user',userRouter)
+router.use('/user', userRouter)
+router.use('/account',accountRouter)
 
 
 
