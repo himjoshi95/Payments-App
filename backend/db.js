47bd4cac8a1c71cd0162e02fee11ecfd8c0e7f02
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-11 20:51:42 +0530

    Step 9 - Created Bank related accountSchema

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+
 
 dotenv.config()
 
@@ -12,9 +13,23 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const User = mongoose.model('User', userSchema);
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required:true
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+const Account = mongoose.model('Account',accountSchema)
 
 
 module.exports = {
-    User
+    User,
+    Account
 };
