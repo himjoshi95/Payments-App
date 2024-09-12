731c34a01a8010005aa90aed53092883e7d8df5f
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-12 20:38:37 +0530

    Integrated backend communication signIn

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/inputBox";
 import { SubHeading } from "../components/SubHeading";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function Signin() {
+    const [username,setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const navigate = useNavigate()
     return (
         <div className="h-screen bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% flex justify-center">
             <div className="flex flex-col justify-center">
@@ -12,11 +19,22 @@ export function Signin() {
                     <Heading label={"Sign In"} />
                     <SubHeading label={"Enter your credentials to access your account"} />
                     
-                    <InputBox label={"Email"} placeholder="doejohn@example.com" />
-                    <InputBox label={"Password"} placeholder="*******" />
+                    <InputBox onChange={(e)=>setUsername(e.target.value)}  label={"Email"} placeholder="doejohn@example.com" />
+                    <InputBox onChange={(e)=>setPassword(e.target.value)} label={"Password"} placeholder="*******" />
 
                     <div className="pt-4" >
-                        <Button label={"Sign In"} onClick={()=>alert("Sign in Clicked")} />
+                        <Button label={"Sign In"} onClick={async () => {
+                            try {
+                                const response = await axios.post("http://localhost:3000/api/v1/user/signin", {
+                                    username,
+                                    password
+                                })
+                                localStorage.setItem("token", response.data.token)
+                                navigate('/dashboard')
+                            } catch (e) {
+                                alert("Something went wrong")
+                            }
+                        }} />
                     </div>
 
                     <BottomWarning label={"Don't have an account?" } buttonText={"Sign Up"} to={"/signup"}/>
