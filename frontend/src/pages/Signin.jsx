0f2c32562cfe40e350a85a278bf0c1deca7043e4
himjoshi95 <himanshujoshi1012@gmail.com>
2024-09-12 14:23:24 +0530

    Added SignIn Page(FE)

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,8 +1,27 @@
+import { BottomWarning } from "../components/BottomWarning";
+import { Button } from "../components/Button";
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/inputBox";
+import { SubHeading } from "../components/SubHeading";
 
 export function Signin() {
     return (
-        <div>
-            Sign in
+        <div className="h-screen bg-slate-400 flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="bg-white w-80 h-max p-2 px-4 text-center rounded-lg">
+                    <Heading label={"Sign In"} />
+                    <SubHeading label={"Enter your credentials to access your account"} />
+                    
+                    <InputBox label={"Email"} placeholder="doejohn@example.com" />
+                    <InputBox label={"Password"} placeholder="*******" />
+
+                    <div className="pt-4" >
+                        <Button label={"Sign In"} onClick={()=>alert("Sign in Clicked")} />
+                    </div>
+
+                    <BottomWarning label={"Don't have an account?" } buttonText={"Sign Up"} to={"/signup"}/>
+                </div>
+            </div>
         </div>
     )
 }
